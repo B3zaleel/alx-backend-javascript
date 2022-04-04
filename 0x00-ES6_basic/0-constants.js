c6e39a5127a9597220e211089f6353ed811a4277
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-04-04 16:23:35 +0100

    :broom: Change quotes used for strings

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,14 +1,14 @@
 export function taskFirst() {
-  const task = "I prefer const when I can.";
+  const task = 'I prefer const when I can.';
   return task;
 }
 
 export function getLast() {
-  return " is okay";
+  return ' is okay';
 }
 
 export function taskNext() {
-  let combination = "But sometimes let";
+  let combination = 'But sometimes let';
   combination += getLast();
 
   return combination;
