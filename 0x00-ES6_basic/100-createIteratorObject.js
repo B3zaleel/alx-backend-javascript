9dca3b42419df4d6929e70747f6df9b00d4ebe84
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-04-04 16:31:34 +0100

    :broom: Enclose generator in parenthesis

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,9 +1,9 @@
 export default function createIteratorObject(report) {
-  return function* () {
+  return (function* _() {
     for (const department of Object.values(report.allEmployees)) {
       for (const employee of department) {
         yield employee;
       }
     }
-  }();
+  }());
 }
