b19a4e97cdc6d6369b4a0b1dad033de2f0c6b444
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-04-04 16:24:13 +0100

    :broom: Remove .js extension in import statement

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,4 +1,4 @@
-import getBudgetObject from './7-getBudgetObject.js';
+import getBudgetObject from './7-getBudgetObject';
 
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
