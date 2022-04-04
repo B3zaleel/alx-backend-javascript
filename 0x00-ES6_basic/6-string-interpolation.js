a2245c1920a9c140f171f27bccdb0fe7ff4e79f8
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-04-04 16:58:38 +0100

    :broom: Fix typos

--- 0x00-ES6_basic/6-string-interpolation.js
+++ 0x00-ES6_basic/6-string-interpolation.js
@@ -6,7 +6,7 @@ export default function getSanFranciscoDescription() {
     capita: '$178,479',
   };
 
-  return `As of ${year}, it was the seventh-highest income county in the United States \
+  return `As of ${year}, it was the seventh-highest income county in the United States\
 , with a per capita personal income of ${budget.income}. As of 2015, San Francisco \
 proper had a GDP of ${budget.gdp}, and a GDP per capita of ${budget.capita}.`;
 }
