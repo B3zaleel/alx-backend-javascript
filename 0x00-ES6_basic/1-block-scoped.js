5d2db9f7ea24711c910c6b360efa451283cda63c
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-04-04 17:13:28 +0100

    :broom: Remove unnecessary statements

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,10 +3,8 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    let task = true;
-    let task2 = false;
-    task = task2;
-    task2 = task;
+    const task = true; // eslint-disable-line no-unused-vars
+    const task2 = false; // eslint-disable-line no-unused-vars
   }
 
   return [task, task2];
