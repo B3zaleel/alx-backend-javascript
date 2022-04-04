4d9548b01269f379696e2ae0c07c39adb742a933
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-04-04 15:46:37 +0100

    :broom: Refactor script

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,5 +1,9 @@
 export default function getBudgetObject(income, gdp, capita) {
-  const budget = { income, gdp, capita, };
+  const budget = {
+    income,
+    gdp,
+    capita,
+  };
 
   return budget;
-};
+}
