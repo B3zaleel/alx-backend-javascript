ec5144242b9109e5b50a11396c8881474ccd0d08
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-04-05 16:31:32 +0100

    :adhesive_bandage: Refactor script

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,10 +1,13 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
+export default async function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
     .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((res) => (
-      res.map((o) => ({ status: o.status, value: o.value || o.reason }))
+      res.map((o) => ({
+        status: o.status,
+        value: o.status === 'fulfilled' ? o.value : o.reason,
+      }))
     ));
 }
