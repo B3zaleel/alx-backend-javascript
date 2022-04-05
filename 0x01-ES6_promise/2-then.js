56679758873cc1c5f49c79308f9ba61bf3fe60bc
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-04-05 16:19:16 +0100

    :adhesive_bandage: Add return to function

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,5 +1,5 @@
 export default function handleResponseFromAPI(promise) {
-  promise
+  return promise
     .then(() => ({ status: 200, body: 'success' }))
     .catch(() => new Error())
     .finally(() => console.log('Got a response from the API'));
