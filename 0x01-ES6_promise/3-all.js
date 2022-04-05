eef5e1161f12f8ec920cc4329178b249baf23df6
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-04-05 16:21:55 +0100

    :adhesive_bandage: Add return to function

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise
+  return Promise
     .all([uploadPhoto(), createUser()])
     .then((res) => {
       console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
