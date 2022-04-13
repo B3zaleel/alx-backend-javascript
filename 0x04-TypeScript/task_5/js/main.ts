51126f124471a2c6a5b0645b52c26a66f39e6ea1
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-04-13 21:44:39 +0100

    :adhesive_bandage: Enclose result of summation in MajorCredits and MinorCredits

--- 0x04-TypeScript/task_5/js/main.ts
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -6,10 +6,10 @@ export interface MinorCredits {
   credits: number & { __brand: 'MinorCredits.credits' };
 }
 
-export function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): number {
-  return subject1.credits + subject2.credits;
+export function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): MajorCredits {
+  return { credits: subject1.credits + subject2.credits } as MajorCredits;
 }
 
-export function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): number {
-  return subject1.credits + subject2.credits;
+export function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): MinorCredits {
+  return { credits: subject1.credits + subject2.credits } as MinorCredits;
 }
