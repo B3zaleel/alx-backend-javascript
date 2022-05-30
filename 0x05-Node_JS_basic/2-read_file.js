a9353a48e6def464100b41a46283ea50be500ed8
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-05-30 19:30:17 +0100

    :wrench: Check file status before reading

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -9,6 +9,9 @@ const countStudents = (dataPath) => {
   if (!fs.existsSync(dataPath)) {
     throw new Error('Cannot load the database');
   }
+  if (!fs.statSync(dataPath).isFile()) {
+    throw new Error('Cannot load the database');
+  }
   const fileLines = fs
     .readFileSync(dataPath, 'utf-8')
     .toString('utf-8')
