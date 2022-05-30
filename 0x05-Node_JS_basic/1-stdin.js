4508471083a2feb0cb0affac6b36e0f68a1559df
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-05-30 20:32:51 +0100

    :wrench: Print closing message in STDERR

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -13,5 +13,5 @@ process.stdin.on('readable', () => {
 });
 
 process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
+  process.stderr.write('This important software is now closing\n');
 });
