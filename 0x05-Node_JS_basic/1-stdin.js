0ca88b3f785350b0e915b198fa277b50d25ace04
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-05-30 20:05:23 +0100

    :wrench: Refactor script

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,16 +1,5 @@
-let userName = '';
-const chunks = [];
-
-process.stdin.on('print_name', () => {
-  let i = 0;
-  userName = chunks.join('');
+process.stdin.on('print_name', (userName) => {
   console.log(`Your name is: ${userName}`);
-  for (; i < chunks.length; i += 1) {
-    chunks.pop();
-  }
-  if (process.stdin.isTTY) {
-    process.exit(0);
-  }
 });
 
 process.stdin.on('end', () => {
@@ -20,23 +9,9 @@ process.stdin.on('end', () => {
 console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('readable', () => {
-  let chunk;
-  do {
-    chunk = process.stdin.read();
-    let i = 0;
-    let canPrintName = false;
-    if (chunk) {
-      for (const code of chunk) {
-        if (code === '\n'.charCodeAt(0)) {
-          canPrintName = true;
-          break;
-        }
-        i += 1;
-      }
-      chunks.push(chunk.slice(0, i).toString());
-      if (canPrintName) {
-        process.stdin.emit('print_name');
-      }
-    }
-  } while (chunk !== null);
+  const chunk = process.stdin.read();
+
+  if (chunk) {
+    process.stdin.emit('print_name', chunk.toString().trim());
+  }
 });
