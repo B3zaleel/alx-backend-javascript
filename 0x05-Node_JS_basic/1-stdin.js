c32583b9fbebb30836d5764def326186d01aacf7
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-05-30 20:07:35 +0100

    :wrench: Change message writing function

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,12 @@
 process.stdin.on('print_name', (userName) => {
-  console.log(`Your name is: ${userName}`);
+  process.stdout.write(`Your name is: ${userName}\n`);
 });
 
 process.stdin.on('end', () => {
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
 
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
   const chunk = process.stdin.read();
