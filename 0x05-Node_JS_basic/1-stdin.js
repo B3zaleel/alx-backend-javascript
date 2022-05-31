e99b1932a0d51171706eb01e51eb0df3b552caf7
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-05-31 02:34:14 +0100

    :wrench: Remove custom event emitting and listening routines

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,10 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.on('print_name', (userName) => {
-  process.stdout.write(`Your name is: ${userName}\n`);
-});
-
 process.stdin.on('readable', () => {
   const chunk = process.stdin.read();
 
   if (chunk) {
-    process.emit('print_name', chunk.toString().trim());
+    process.stdout.write(`Your name is: ${userName}\n`);
   }
 });
 
