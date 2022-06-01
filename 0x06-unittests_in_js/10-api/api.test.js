5d93dbf6baebdbe306c0792bb6ce1d9ca95dd6aa
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-06-01 20:58:18 +0100

    :broom: Refactor script

--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -45,7 +45,8 @@ describe('API integration test', () => {
   it('GET /available_payments returns valid response', (done) => {
     request.get(`${API_URL}/available_payments`, (_err, res, body) => {
       expect(res.statusCode).to.be.equal(200);
-      expect(JSON.parse(body)).to.be.deep.equal({payment_methods: {credit_cards: true, paypal: false}});
+      expect(JSON.parse(body))
+        .to.be.deep.equal({payment_methods: {credit_cards: true, paypal: false}});
       done();
     });
   });
