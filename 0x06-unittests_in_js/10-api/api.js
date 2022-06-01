3878baaec3bd6a5277a45d9bc3306c85a0c9fa62
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-06-01 21:00:41 +0100

    :wrench: Remove safe navigation operator

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -20,7 +20,11 @@ app.get('/available_payments', (_req, res) => {
 });
 
 app.post('/login', (req, res) => {
-  const username = req.body?.userName || '';
+  let username = '';
+
+  if (req.body) {
+    username = req.body.userName;
+  }
 
   res.send(`Welcome ${username}`);
 });
